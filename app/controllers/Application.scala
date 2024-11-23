c82b57495cd815839b4088caaf4ac57a93507050
Peter Knipp <peteraknipp@gmail.com>
2024-11-23 18:42:33 -0500

    Decrease # of cells by 10 squared.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -92,7 +92,7 @@ class Application @Inject()(val controllerComponents: ControllerComponents, val 
   }
 
   def results(path: String) = Action {
-    Ok(views.html.results(path, new Grid(400.0, 100)))
+    Ok(views.html.results(path, new Grid(400.0, 10)))
   }
 
   def db(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
